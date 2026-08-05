31e54122003aca29aacfb54767fd15351528a0c4
Samadhanautomation <samadhanautomation@gmail.com>
2026-08-05 17:35:12 +0530

    changed the config file

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 // Playwright configuration file
 // Controls browser, baseURL, retries, reporter, etc.
 
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
@@ -13,5 +13,21 @@ export default defineConfig({
     screenshot: 'on',
     video: 'retain-on-failure'
   },
+ 
+   projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+
   reporter: [['list'], ['./configure/reporterConfig.js']]
 });
